5ea97b4090b43a7c9e48f6ae97f98413dea4dc11
Enmanuel <enmanuelle.hernandez@gmail.com>
2026-07-08 15:13:39 -0600

    no translate

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="es" translate="no">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="google" content="notranslate">
 
         <title inertia>{{ config('app.name', 'STJ Dashboard') }}</title>
         <link rel="icon" type="image/png" href="https://stj-assets.sfo3.cdn.digitaloceanspaces.com/logos/stjdashboard/favicons%20st%20jacks_favicon%20dashboard.png">
