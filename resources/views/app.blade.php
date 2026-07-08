75347e73d0b5666cffe70c290884da719a57498f
Enmanuel <enmanuelle.hernandez@gmail.com>
2026-07-08 11:18:36 -0600

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title inertia>{{ config('app.name', 'STJ Dashboard') }}</title>
+        <link rel="icon" type="image/png" href="https://stj-assets.sfo3.cdn.digitaloceanspaces.com/logos/stjdashboard/favicons%20st%20jacks_favicon%20dashboard.png">
+        <link rel="shortcut icon" type="image/png" href="https://stj-assets.sfo3.cdn.digitaloceanspaces.com/logos/stjdashboard/favicons%20st%20jacks_favicon%20dashboard.png">
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @inertiaHead
